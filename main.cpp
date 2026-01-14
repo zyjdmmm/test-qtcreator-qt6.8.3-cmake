0895ee5c0b46620b2f5916c37ecfcc3b9e7c6dc8
zyjdmmm <75295100+zyjdmmm@users.noreply.github.com>
2026-01-14 10:25:20 +0800

    编译

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include<qstring.h>
 
 int main(int argc, char *argv[])
 {
+    QString b="hello";
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
